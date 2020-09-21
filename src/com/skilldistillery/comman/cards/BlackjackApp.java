23a93b88b4cebbb92550339b5fbd29ddf5860451
Bronson Spies <spiesbronson@gmail.com>
2020-09-20 18:11:02 -0600

    working app, per requirements .

--- src/com/skilldistillery/comman/cards/BlackjackApp.java
+++ src/com/skilldistillery/comman/cards/BlackjackApp.java
@@ -23,13 +23,17 @@ public class BlackjackApp {
 
 	private void playerMenu() {
 		int choice = 0;
-		try {
 			while (keepgoing) {
 				System.out.println("What would you like to do?");
 				System.out.println("1. Hit");
 				System.out.println("2. Stay");
 			
+				try {
 					choice = kb.nextInt();
+				} catch (Exception e) {
+					kb.nextLine();
+					System.out.println("please enter a valid number");
+				}
 			
 				if (choice == 1) {
 					player.playerHand.addCard(dealer.dealCard());
@@ -38,6 +42,7 @@ public class BlackjackApp {
 						System.out.println("Player wins!");
 						playAgain();
 					} else if (player.playerHand.isBust()) {
+						System.out.println("Player busted");
 						System.out.println("dealer wins");
 						playAgain();
 					}
@@ -46,33 +51,31 @@ public class BlackjackApp {
 					keepgoing = false;
 				}
 				
-			}
-		} catch (Exception e) {
-			System.out.println("please enter a valid number");
-			kb.nextInt();
 		}
 	}
 
 	private void playAgain() {
 		System.out.println("would you like to play again?");
 		int choice = 0;
+		while(choice != 2) {
 		System.out.println("enter 1 to play again and 2 to quit");
 		try {
 			choice = kb.nextInt();
+			
+		} catch (InputMismatchException e) {
+			kb.nextLine();
+			System.out.println("please enter 1 to play agian or 2 to quit");
+		}
 		if (choice == 1) {
 			player.playerHand.clear();
 			dealer.dealerHand.clear();
 			b.run();
 		}
-		else if(choice == 2){
+		}
+		if(choice == 2){
 			keepgoing = false;
 		}
-		} catch (InputMismatchException e) {
-			System.out.println("please enter 1 to play agian or 2 to quit");
-			kb.nextInt();
-		}
 	}
-
 	private void dealerTurn() {
 		while (dealer.dealerHand.getHandValue() < 17) {
 			System.out.println("hit");
@@ -106,26 +109,41 @@ public class BlackjackApp {
 	}
 	
 	private void checkWinner() {
-	if(player.playerHand.getHandValue() <= dealer.dealerHand.getHandValue()) {
-		System.out.println("Dealer wins");
-		playAgain();
-	}
-	else if(player.playerHand.getHandValue() > dealer.dealerHand.getHandValue()) {
-		System.out.println("Player wins");
-		playAgain();
-	} else {
-		
-	}
-		
+	
+		if(player.playerHand.getHandValue() <= dealer.dealerHand.getHandValue()) {
+			System.out.println("Dealer wins");
+			playAgain();
+		}
+		else if(player.playerHand.getHandValue() > dealer.dealerHand.getHandValue()) {
+			System.out.println("Player wins");
+			playAgain();
+		}
 	}
 
 	private void startGame() {
 
-		System.out.println("Welcome");
+		System.out.println("********************");
+		System.out.println("Welcome to Blackjack");
+		System.out.println("********************");
 		player.addCardToHand(dealer.dealCard());
 		player.addCardToHand(dealer.dealCard());
+		if(player.playerHand.isBust()) {
+			System.out.println("2 aces");
+			b.run();
+			
+		}
+		if(player.playerHand.isBlackjack()) {
+			System.out.println("21, player wins");
+			playAgain();
+		}
 		dealer.addCardToHand(dealer.dealCard());
 		dealer.addCardToHand(dealer.dealCard());
+		if(dealer.dealerHand.isBust()) {
+			System.out.println("2 aces");
+			b.run();
+			
+		}
+		
 		System.out.println(player.playerHand.toString());
 		System.out.println(player.playerHand.getHandValue());
 		dealer.displayCard();
